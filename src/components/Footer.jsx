793b492377b4a9680a397e8820745a39f329fe13
rrishiddh <rrishiddh@gmail.com>
2024-11-03 22:01:28 +0600

    navbar and footer setup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
 
 const Footer = () => {
-    return (
-        <div>
-            <h1>footer</h1>
-        </div>
-    );
+  return (
+    <div className="max-w-screen-2xl py-16">
+      <div className="w-[60%] mx-auto  text-center">
+        <footer className=" space-y-4   mx-auto">
+          <h1 className="text-2xl lg:text-4xl font-bold">Gadget Heaven Zone</h1>
+          <p className="font-thin">
+            Leading the way in cutting edge technology and innovation
+          </p>
+        </footer>
+        <div className="divider "></div>
+
+        <footer className=" footer text-base-content lg:ml-20 py-4  mx-auto">
+          <nav>
+            <h6 className="footer-title">Services</h6>
+            <a className="link link-hover">Product Support</a>
+            <a className="link link-hover">Order Tracking</a>
+            <a className="link link-hover">Shipping and Delivery</a>
+            <a className="link link-hover">Returns</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">Company</h6>
+            <a className="link link-hover">About us</a>
+            <a className="link link-hover">Contact</a>
+            <a className="link link-hover">Career</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">Legal</h6>
+            <a className="link link-hover">Terms of use</a>
+            <a className="link link-hover">Privacy policy</a>
+            <a className="link link-hover">Cookie policy</a>
+          </nav>
+        </footer>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,66 @@
-import React from 'react';
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h2>nav</h2>
-        </div>
+    const navLink = (
+        < >
+          <li>
+            <NavLink to='/'>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='/'>Statistics</NavLink>
+          </li>
+          <li>
+            <NavLink to='/'>Dashboard</NavLink>
+          </li>        
+        </>
     );
+
+  return (
+    <div className="max-w-screen-2xl">
+      <div className="navbar bg-base-100 w-[94%] mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-gray-300 rounded-box z-[1] mt-3 w-52 p-2 shadow space-y-4 "
+            >
+            {
+                navLink
+            }        
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">Gadget Heaven Zone</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 space-x-4">
+          {
+                navLink
+            }           
+          </ul>
+        </div>
+        <div className="navbar-end gap-4">
+          <a className=" rounded-full bg-black/30 p-2"><img className="w-5 rounded-full h-5" src="https://img.icons8.com/?size=100&id=85080&format=png&color=000000" alt="" /></a>
+          <a className="rounded-full bg-black/30 p-2"><img className="w-5 rounded-full h-5" src="https://img.icons8.com/?size=100&id=85038&format=png&color=000000" alt="" /></a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
